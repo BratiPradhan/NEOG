b05205f820ee1f87ec872f2da019c78849b6c2ba
BratiPradhan <pradhanbrati@gmail.com>
2021-03-14 11:11:07 +0530

    NEOG-BUILD-LOGIC-001: solved three intermediate questions
    001-operators-branching-loops

--- build-logic/001_operators_branching_loops/index.js
+++ build-logic/001_operators_branching_loops/index.js
@@ -14,11 +14,11 @@ const numbers = [5, 13, 7, 21, 48]
 // Write a program to take a number input from user and determine whether the number is odd or even.
 const userInput = document.getElementById("userInput")
 
-userInput.addEventListener('change', (event) => {
-    const userInputValue = event.target.value;
+// userInput.addEventListener('change', (event) => {
+//     const userInputValue = event.target.value;
 
-    alert(`${userInputValue%2 === 0 ? `${userInputValue} is even` : `${userInputValue} is odd`}`)
-} )
+//     alert(`${userInputValue%2 === 0 ? `${userInputValue} is even` : `${userInputValue} is odd`}`)
+// } )
 
 
 // MAXIMUM AND MINIMUM OUT OF TWO GIVEN NUMBERS
@@ -58,4 +58,56 @@ const onChange = (e) => {
     }
 }
 month.addEventListener('change', onChange )
+
+// Intermediate
+// Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
+
+let arrayFizzBuzz = []
+
+for (i=1; i<=100; i++) {
+    let output = ''
+    if(i%3 === 0 && i%5 ===0) {
+        output = 'FizzBuzz'
+
+    } else if(i%3 === 0 && i%5 !==0) {
+        output = 'Fizz'
+
+    } else if(i%3 !== 0 && i%5 ===0) {
+        output = 'Buzz'
+
+    } else {
+        output = i
+    }
+    arrayFizzBuzz.push(output)
+}
+console.log(arrayFizzBuzz)
+
+
+// Print the following star pattern :-
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+let n = 5;
+for(i=0; i <= n; i++  ) {
+    console.log('* '.repeat(i))
+}
+
+// Write a program to take a number input from user and print multiplication table 12 times for that number.
+const onInput = (event) => {
+    let userInput = event.target.value;
+    let n = 12;
+    for(let i=1; i<=n; i++){
+        let newUserInput = userInput* i
+
+        console.log(newUserInput)
+    }
+    console.log('\n')
+    
+}
+
+userInput.addEventListener('input', onInput)
 
